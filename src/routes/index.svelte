5ef30f5f9ccb1368d8ddcdee0efa76de8cfcd8fe
rongying <rongying.koh@thoughtworks.com>
2020-09-18 18:33:57 +0800

    Add update collection name method

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import CollectionApi from "../api/collection";
   import Modal from '../components/modal/Modal.svelte'
+  import SvgIcon from '../svg/SvgIcon.svelte'
 
   let userId: string = 'sprNjQEcRv';
   let collections: Array<{ objectId: string, name: string, user: { __type: string, className: string, objectId: string }}> = [];
@@ -17,19 +18,45 @@
     openModal = false
     collections = await CollectionApi.getCollections(userId);
   }
+
+  const handleUpdate = (collectionId, updatedName) => async () => {
+    const body = {
+      name: updatedName
+    }
+    await CollectionApi.updateCollection(collectionId, body)
+    collections = await CollectionApi.getCollections(userId);
+  }
   
   const modalSetVisible = (visible) => () => openModal = visible; 
-
-
 </script>
+
+<style>
+.card {
+  background: rgb(232, 231, 231);
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+</style>
 
 <h1>notes-app</h1>
 <p>user id is {userId}</p>
 
 <button on:click={modalSetVisible(true)}>Add Collection</button>
 {#if collections}
-  {#each collections as collection}
-    <p>{collection.name}</p>
+  {#each collections as collection, index}
+  <div class="card">
+    {#if collection.isUpdating}
+      <SvgIcon iconType='check' strokeColor='#4CAF50' onClick={handleUpdate(collection.objectId, collection.name)} />
+      <input bind:value={collection.name} />
+      <span on:click={() => collections[index].isUpdating=false}>Cancel</span>
+    {:else}
+      <SvgIcon iconType='folder' />
+      <span on:click={() => collections[index].isUpdating=true}>{collection.name}</span>
+    {/if}
+  </div>
   {/each}
 {/if}
 
---
+++ src/svg/SvgIcon.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let strokeColor = "#607D8B";
+  export let iconType = 'link';
+
+  const doNothing = () => {};
+  export let onClick = doNothing;
+</script>
+
+<svg on:click={onClick} xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke={strokeColor} fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z"/>
+{#if iconType == 'check'}
+  <path d="M5 12l5 5l10 -10" />
+{:else if iconType == 'link'}
+  <path d="M10 14a3.5 3.5 0 0 0 5 0l4 -4a3.5 3.5 0 0 0 -5 -5l-.5 .5" />
+  <path d="M14 10a3.5 3.5 0 0 0 -5 0l-4 4a3.5 3.5 0 0 0 5 5l.5 -.5" />
+{:else if iconType == 'folder'}
+  <path d="M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2" />
+{/if}
+</svg>
